7c8f48fdaea232fcdcde98e759fa463a76b9b2eb
Aastha Lakhatariya <aasthalakhatariya7@gmail.com>
2026-09-22 18:39:32 +0530

    Time: 1 ms (92.12%), Space: 45.2 MB (88.34%) - LeetHub

--- 3232-find-if-digit-game-can-be-won/3232-find-if-digit-game-can-be-won.java
+++ 3232-find-if-digit-game-can-be-won/3232-find-if-digit-game-can-be-won.java
@@ -4,15 +4,15 @@ class Solution {
         int singleDigitSum = 0;
         int doubleDigitSum = 0;
 
-        for (int i : nums) {
+        for (int i = 0; i < nums.length; i++) {
 
-            if (i <= 9) {
+            if (nums[i] <= 9) {
 
-                singleDigitSum += i;
+                singleDigitSum += nums[i];
 
             } else {
 
-                doubleDigitSum += i;
+                doubleDigitSum += nums[i];
 
             }
 
