319d46223c0ef3d5c4703f6339ea71325058a6e7
Aastha Lakhatariya <aasthalakhatariya7@gmail.com>
2026-09-22 19:49:09 +0530

    Time: 19 ms (3.37%), Space: 43.9 MB (5.5%) - LeetHub

--- 0682-baseball-game/0682-baseball-game.java
+++ 0682-baseball-game/0682-baseball-game.java
@@ -1,40 +1,78 @@
 class Solution {
     public int calPoints(String[] operations) {
 
-        Stack<Integer> score = new Stack<Integer>();
+        int scoreArr[] = new int[operations.length];
+        int top = -1;
         int sum = 0;
 
         for (String opr : operations) {
 
             switch (opr) {
                 case "+":
-                    int prevSum = score.peek() + score.get(score.size() - 2);
-                    score.push(prevSum);
+                    int prevSum = scoreArr[top] + scoreArr[top - 1];
+                    // score.push(prevSum);
+                    if (top == scoreArr.length - 1) {
+
+                        break;
+
+                    } else {
+
+                        top++;
+                        scoreArr[top] = prevSum;
+
+                    }
+
                     break;
 
                 case "D":
-                    int doublePrevRes = score.peek() * 2;
-                    score.push(doublePrevRes);
+                    int doublePrevRes = scoreArr[top] * 2;
+                    if (top == scoreArr.length - 1) {
+
+                        break;
+
+                    } else {
+
+                        top++;
+                        scoreArr[top] = doublePrevRes;
+
+                    }
                     break;
 
                 case "C":
-                    score.pop();
+
+                    if (top == -1) {
+
+                        break;
+
+                    } else {
+
+                        top--;
+
+                    }
+
                     break;
 
                 default:
-                    score.push(Integer.parseInt(opr));
+                    if (top == scoreArr.length - 1) {
 
+                        break;
+
+                    } else {
+
+                        top++;
+                        scoreArr[top] = Integer.parseInt(opr);
+
+                    }
             }
 
         }
 
-        for (Integer s : score) {
+        for (int i = 0; i <= top; i++) {
 
-            sum += s;
+            sum += scoreArr[i];
+            System.out.println(scoreArr[i] + " ");
 
         }
-
-        System.out.println(score);
 
         return sum;
 
